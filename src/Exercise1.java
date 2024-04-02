7831bd835e6b53f25b48af8e7c16882ebc537b0e
Ronoquade Lawrencea <rlawrence2@ggc.edu>
2024-04-02 17:16:22 -0400

    initial commit

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Exercise1 {
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Exercise1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // While-Loop boolean control variable
+        boolean goodIntGiven = false;
+
+        int sum = 0;
+
+        while (!goodIntGiven) {
+                try {
+                    sum = 0;
+                    System.out.print("Enter two integers: ");
+                    sum += input.nextInt();
+                    sum += input.nextInt();
+                    goodIntGiven = true;
+                }
+                catch (InputMismatchException e){
+                    input.nextLine();
+                    System.out.println("Sorry, either one or both inputs weren't integers...");
+                }
+            }
+            System.out.println(sum);
+    }
 }
--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Exercise2 {
+import java.util.Random;
+import java.util.Scanner;
+public class Exercise2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Creating the array of double values and filling
+        // all the indices with random double values.
+        double[] randomArray = new double[50];
+
+        Random randNum = new Random();
+
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = 10 * randNum.nextDouble();
+        }
+        // While-Loop with boolean loop control variable and exception catcher
+        boolean validIndex = false;
+        double returnedElement = 0;
+        while (!validIndex) {
+
+            try {
+                System.out.print("Enter the index of the 50 elements you want to view (0-49): ");
+                returnedElement = randomArray[input.nextInt()];
+                validIndex = true;
+            }
+            catch (ArrayIndexOutOfBoundsException e){
+                input.nextLine();
+                System.out.println("Sorry, the index is out of range. Try again.");
+            }
+
+        }
+        System.out.println(returnedElement);
+    }
 }
